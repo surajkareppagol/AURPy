c05f8dd048c3907940518fd31ca1d589f4df6d3a
surajkareppagol <surajkareppagol.dev@gmail.com>
2023-10-30 17:58:24 +0530

    📄 Refactor code, update functionality

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # AUR Helper Py
 
 `AUR Helper Py` is another AUR helper for you, not full feature rich just does one thing and that is to download the correct package, for search it uses the `Aurweb RPC interface`.
-
-Then there is just some simple python, although it is still in development you can use it, but i highly recommend to not use it, because as i said it's not completely built yet.
-
-## This is how you can run it
-
-![Aur Helper Py](assets/aur_arch_py.gif)
--- aur_helper_py.py
+++ aur_helper_py.py
@@ -1,16 +1,47 @@
-import requests
-import os
-import subprocess
 import json
+import os
+import re
+import requests
+import subprocess
+import sys
 
-package_name = input("Enter the package name: ")
+pkg_name = input("📦️ Package: ")
 
-response = json.loads(requests.get(f"https://aur.archlinux.org/rpc/?v=5&type=info&arg[]={package_name}").text)
+pkg_response = requests.get(
+    f"https://aur.archlinux.org/rpc/?v=5&type=search&arg={pkg_name}"
+)
 
-if(response["resultcount"] == 0):
-  print(f"Error: Couldn't able to find package \"{package_name}\"")
-elif(response["results"][0]["Name"] == package_name):
-  subprocess.run(["git", "clone", f"https://aur.archlinux.org/{package_name}.git"])
-  os.chdir(f"{package_name}")
-  subprocess.run(["makepkg", "-si"])
+pkg_json = json.loads(pkg_response.text)
 
+# Install Package
+
+
+def install_pkg(pkg_name):
+    subprocess.run(["git", "clone", f"https://aur.archlinux.org/{pkg_name}.git"])
+    os.chdir(pkg_name)
+    subprocess.run(["makepkg", "-si"])
+
+
+# If No Packages Found Exit
+
+if pkg_json["resultcount"] == 0:
+    print(f'🚫 Package "{pkg_name}" Not Found.')
+    sys.exit(1)
+elif pkg_json["resultcount"] == 1:
+    if pkg_json["results"][0]["Name"] == pkg_name:
+        install_pkg(pkg_name)
+else:
+    pkg_name_regex = re.compile(f"{pkg_name}")
+
+    # Print All The Available Packages
+
+    for index, pkg in enumerate(pkg_json["results"], start=1):
+        if pkg_name_regex.search(pkg["Name"]) != None:
+            print(f"{index} : {pkg['Name']} - {pkg['Description']}")
+
+    # Select A Package To Download
+
+    selected_pkg = int(
+        input("\n📦️ Found Multiple Packages, Please Select (1, 2, ..): ")
+    )
+    install_pkg(pkg_json["results"][selected_pkg - 1]["Name"])
