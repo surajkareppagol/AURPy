781bfd4ddf984d0b91f009d02662c789d40b877a
surajkareppagol <surajkareppagol.dev@gmail.com>
2023-11-15 12:11:58 +0530

    🌟 Refactor code, use functions instead of class

--- src/aurpy/aurpy.py
+++ src/aurpy/aurpy.py
@@ -9,83 +9,111 @@ from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
 
-console = Console()
-table = Table(title="📦 Packages")
 
-console.print(Panel("Arch AUR Py"))
-
-table.add_column("ID", justify="center")
-table.add_column("Name", justify="center")
-table.add_column("Description", justify="center")
-
-
-def error_text(msg):
+# Util Functions
+def error_text(msg: str) -> str:
     return f"\n[red bold]Error[/red bold]: {msg}"
 
 
-def success_text(msg):
+def success_text(msg: str) -> str:
     return f"[bright_green bold]Success[/bright_green bold]: {msg}"
 
 
-class AUR:
-    def __init__(self, pkg_name):
-        self.pkg_name = pkg_name
-        self.pkg_url = f"https://aur.archlinux.org/rpc/?v=5&type=search&arg={pkg_name}"
+def search_pkg(url: str) -> dict | int:
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException:
+        console.print(error_text("🚫 Something Went Wrong."))
+        sys.exit(1)
 
-        self.get_json()
+    pkg_info = json.loads(response.text)
 
-    def get_json(self):
-        response = requests.get(self.pkg_url)
-        self.pkg_info = json.loads(response.text)
+    if response.status_code == 200:
+        return pkg_info
+    return 0
 
-    def install_pkg(self, pkg_name):
-        pkg_aur_url = f"https://aur.archlinux.org/{pkg_name}.git"
-        subprocess.call(["git", "clone", pkg_aur_url])
-        os.chdir(pkg_name)
-        subprocess.call(["makepkg", "-si"])
 
-    def search_and_install_pkg(self):
-        total_pkgs = self.pkg_info["resultcount"]
+def create_table(packages: int) -> Table:
+    table = Table(title=f"📦 Found {packages} Packages", title_style="bold")
+    table.add_column("ID", justify="left")
+    table.add_column("Name", justify="left")
+    table.add_column("Description", justify="left")
+    return table
 
-        console.print(success_text(f"Found {total_pkgs} Package's."))
+def create_pkg_name(start:int, end:int, pkg_name: str) -> str:
+    return f"[white]{pkg_name[:start]}[/white][green_yellow]{pkg_name[start:end + 1]}[/green_yellow][white]{pkg_name[end + 1:]}[/white]"
 
-        if total_pkgs == 0:
-            console.print(error_text(f'🚫 Package "{self.pkg_name}" Not Found.'))
-            sys.exit(1)
-        elif total_pkgs == 1 and self.pkg_info["results"][0]["Name"] == self.pkg_name:
-            self.install_pkg(self.pkg_name)
-        else:
-            pkg_name_regex = re.compile(f"{self.pkg_name}")
+# Set Console
+console = Console()
+console.print(Panel("Arch AUR Py"))
 
-            for index, pkg in enumerate(self.pkg_info["results"], start=1):
-                pkg_content_match = pkg_name_regex.search(pkg["Name"])
-                description = (
-                    pkg["Description"]
-                    if pkg["Description"] is not None
-                    else "No Description Found"
-                )
-                if pkg_content_match is not None:
-                    table.add_row(
-                        f"{index}",
-                        f"[green_yellow]{pkg['Name']}[/green_yellow]",
-                        f"{description}",
-                    )
-            console.print(table)
 
-            selected_pkg = int(
-                console.input("\n📦 Select One Package (1 or 2 or ...): ")
+# Install Package
+def build_and_install(pkg_name: str) -> None:
+    try:
+        os.mkdir(f"{os.getenv("HOME")}/.aurpy")
+    except FileExistsError:
+        pass
+
+    os.chdir(f"{os.getenv("HOME")}/.aurpy")
+    pkg_aur_url = f"https://aur.archlinux.org/{pkg_name}.git"
+    subprocess.call(["git", "clone", pkg_aur_url])
+    os.chdir(pkg_name)
+    subprocess.call(["makepkg", "-si"])
+
+
+def install_pkg(pkg_info: dict) -> None:
+    total_pkgs = pkg_info["resultcount"]
+    pad = len(str(total_pkgs))
+
+    if not total_pkgs:
+        console.print(error_text(f'🚫 Package "{pkg_name}" Not Found.'))
+        sys.exit(1)
+
+    table = create_table(total_pkgs)
+
+    pkg_name_regex = re.compile(f"{pkg_name}")
+
+    for index, pkg in enumerate(pkg_info["results"], start=1):
+        pkg_content_match = pkg_name_regex.search(pkg["Name"])
+
+        description = (
+            pkg["Description"]
+            if pkg["Description"] is not None
+            else "No Description Found"
+        )
+
+        if pkg_content_match is not None:
+            table.add_row(
+                f"{index}".zfill(pad),
+                create_pkg_name(pkg_content_match.start(), pkg_content_match.end(), pkg["Name"]),
+                f"{description}",
             )
-            selected_pkg_to_install = self.pkg_info["results"][selected_pkg - 1]["Name"]
-            self.install_pkg(selected_pkg_to_install)
+
+    console.print(table)
+
+    selected_pkg = int(console.input("\n📦 Select One Package (1 or 2 or ...): "))
+
+    selected_pkg_to_install = pkg_info["results"][selected_pkg - 1]["Name"]
+
+    build_and_install(selected_pkg_to_install)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        console.print(error_text("Provide A Package Name."))
-        sys.exit()
-
     try:
-        aur = AUR(sys.argv[1])
-        aur.search_and_install_pkg()
+        if len(sys.argv) == 1:
+            console.print(error_text("Provide A Package Name."))
+            sys.exit()
+
+        pkg_name = sys.argv[1]
+        pkg_url = f"https://aur.archlinux.org/rpc/?v=5&type=search&arg={pkg_name}"
+
+        pkg_json = search_pkg(pkg_url)
+
+        if not pkg_json:
+            console.print(error_text("🚫 Something Went Wrong."))
+            sys.exit(1)
+
+        install_pkg(pkg_json)
     except KeyboardInterrupt:
         console.print(error_text("Terminated With CTRL + C."))
